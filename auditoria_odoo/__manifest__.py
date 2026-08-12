e4fc0a2d6811852c5b7d41634aa17dd57554a307
nicoleviarengo <nicoleviarengo@gmail.com>
2026-08-12 17:57:53 -0300

    me equivoque de usuario jeje

--- auditoria_odoo/__manifest__.py
+++ auditoria_odoo/__manifest__.py
@@ -1,7 +1,7 @@
 {
     "name": "Auditoría de Actividad Odoo",
     "version": "19.0.1.0.0",
-    "summary": "Auditoría interna y aprovechamiento de los registros de actividad del ERP",
+    "summary": "Auditoría interna y aprovechamiento de los registros de actividad de Odoo",
     "author": "Gabriel Bula, Giuliano Galván, Nicole Viarengo",
     "website": "https://github.com/GabrielB97/auditoria_odoo",
     "license": "LGPL-3",
